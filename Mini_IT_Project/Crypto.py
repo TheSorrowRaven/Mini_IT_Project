95b686b240f44b1b0b3b8d726883d4b1cea57443
BuildTools <unconfigured@null.spigotmc.org>
2020-04-23 21:54:44 +0800

    Added permission : "Balance Checking"

--- Mini_IT_Project/Crypto.py
+++ Mini_IT_Project/Crypto.py
@@ -3,5 +3,7 @@ from luno_python.client import Client
 #Authorise for crypto usage
 data = Client(api_key_id='mjy945hg9xyyn', api_key_secret='x02-ibaP-mkHPrMLeIeiParg_vCf59LbE-76pkVQiq0')
 
+#Permissions
+Perm_R_Balance = 1
+
 #View the balance of crypto
-print('codecheck')
