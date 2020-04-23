1084ba4a6b6df614000baaac57925c0877c9a9ba
BuildTools <unconfigured@null.spigotmc.org>
2020-04-23 12:01:07 +0800

    Organise api data

--- Mini_IT_Project/Crypto.py
+++ Mini_IT_Project/Crypto.py
@@ -1,4 +1,6 @@
-apikeyid = 'mjy945hg9xyyn'
-apikeysecret = 'x02-ibaP-mkHPrMLeIeiParg_vCf59LbE-76pkVQiq0'
+from luno_python.client import Client
+
+#Authorise for crypto usage
+data = Client(api_key_id='mjy945hg9xyyn', api_key_secret='x02-ibaP-mkHPrMLeIeiParg_vCf59LbE-76pkVQiq0')
 
 #View the balance of crypto
