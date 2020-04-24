91a8363b8bfd98df1f3afa8e1e52308055052d6c
BuildTools <unconfigured@null.spigotmc.org>
2020-04-25 00:46:51 +0800

    Fully working Crypto Balance Checker ( BTC , ETH and XRP ) !

--- Mini_IT_Project/Crypto.py
+++ Mini_IT_Project/Crypto.py
@@ -2,9 +2,17 @@ from luno_python.client import Client
 
 #App function
 
-def CryptoBalance():
-    balance = data.get_balances()
-    print(balance)
+def CryptoBalance(picker):
+    json_data = data.get_balances()
+    if picker == 1:        
+        coin = json_data['balance'][0]['balance']
+        print('Your ethereum balance is :', coin, 'ETH')
+    elif picker == 2:
+        coin = json_data['balance'][2]['balance']
+        print('Your bitcoin balance is :', coin, 'BTC')
+    elif picker == 3:
+        coin = json_data['balance'][3]['balance']
+        print('Your ripple balance is :', coin, 'XRP')
 
 #Authorization with the Luno API
 user_credentials = {}                  #Allow user prompt only if theres no data
@@ -27,5 +35,12 @@ Perm_R_Balance = 1
 #Main Screen
 do_something = int(input('Press 1 for crypto balance: '))
 
+#Balance Checker
 if do_something == 1:
-    CryptoBalance()
+    while True:
+        picker = int(input('Press 1 for Ethereum, Press 2 for Bitcoin , Press 3 for Ripple'))
+        if picker < 4:
+            CryptoBalance(picker)
+        else:
+            break
+        
