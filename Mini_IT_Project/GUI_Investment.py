344ae7fd04d956caba9b4e39bae010a02f995c2c
BuildTools <unconfigured@null.spigotmc.org>
2020-05-03 23:34:18 +0800

    owo new commmit who dis

--- Mini_IT_Project/GUI_Investment.py
+++ Mini_IT_Project/GUI_Investment.py
@@ -9,12 +9,13 @@ class GUI_Investment:
         self.investmentDesc.place(x = 600, y = 300, anchor = "center")
         self.investmentDesc2 = Label(master = parent, text = "Please signup first from the Luno Website", font = ("", 24), bg = Constants.mainWindowBgColor)
         self.investmentDesc2.place(x = 600, y = 360, anchor = "center")
-        self.proceedBtn = Button(master = parent, text = "Proceed", font = ("", 24), bg = Constants.mainWindowBgColor) #might change bg in a later date
+        self.proceedBtn = Button(master = parent, text="Proceed") #might change bg in a later date
         self.proceedBtn.place(x = 660, y = 420, anchor = "center")
         pass
 
     def LoginMenu(self, parent: Frame):
-
+        self.investmentDesc = Label(master = parent, text = "NOTE: If you don't have a Luno Account", font = ("", 24), bg = Constants.mainWindowBgColor)
+        self.investmentDesc.place(x = 600, y = 100, anchor = "center")
         pass
 
     def MainScreen(self, parent: Frame):
