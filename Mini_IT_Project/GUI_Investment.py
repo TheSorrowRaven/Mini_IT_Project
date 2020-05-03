01bfbc7be45d20ca844550de4494f102cb3ad3f8
BuildTools <unconfigured@null.spigotmc.org>
2020-05-03 10:22:40 +0800

    UI Desc Implemented

--- Mini_IT_Project/GUI_Investment.py
+++ Mini_IT_Project/GUI_Investment.py
@@ -1,11 +1,15 @@
 # Raven
-from tkinter import Frame
+from tkinter import Frame, Label, Button, Canvas, PhotoImage
 import Constants
 
 class GUI_Investment:
 
     def __init__(self, parent: Frame):
-        
+        self.investmentDesc = Label(master = parent, text = "NOTE: If you don't have a Luno Account", font = ("", 24), bg = Constants.mainWindowBgColor)
+        self.investmentDesc.place(x = 600, y = 300, anchor = "center")
+        self.investmentDesc2 = Label(master = parent, text = "Please signup first from the Luno Website", font = ("", 24), bg = Constants.mainWindowBgColor)
+        self.investmentDesc2.place(x = 600, y = 365, anchor = "center")
+
         
 
         pass
--- Mini_IT_Project/GUI_Planner.py
+++ Mini_IT_Project/GUI_Planner.py
@@ -5,8 +5,6 @@ import Constants
 class GUI_Planner:
 
     def __init__(self, parent: Frame):
-        self.investmentDesc = Label(master = parent, text = "InvestmentERROR", font = ("", 36), bg = Constants.mainWindowBgColor)
-        self.investmentDesc.place(anchor = "center")
         
 
         pass
