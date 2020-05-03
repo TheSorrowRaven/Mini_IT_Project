50e336ac93978244e50667464afd808718114ab3
darwisy01 <darwisy01@gmail.com>
2020-05-03 15:05:27 +0800

    edit

--- Mini_IT_Project/GUI_Planner.py
+++ Mini_IT_Project/GUI_Planner.py
@@ -6,7 +6,7 @@ class GUI_Planner:
 
     def __init__(self, parent: Frame):
         self.plannerDesc = Label(master = parent, text = "This platform all about your budget planner", font = ("", 12), bg = Constants.mainWindowBgColor)
-        self.plannerDesc.place(relx = 0.5 , rely = 2, anchor = "n")
+        self.plannerDesc.place(relx = 0.5 , rely = 0, anchor = "n")
         self.plannerDesc2 = Label(master = parent, text = "\'The safe way to double your money is to fold it over once and put it in your pocket\'", font = ("", 12), bg = Constants.mainWindowBgColor)
         self.plannerDesc2.place(relx = 1.0, rely = 1.0, anchor = "se")
         
