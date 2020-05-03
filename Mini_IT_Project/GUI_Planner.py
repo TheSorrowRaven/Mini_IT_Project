33e7c4b6ae91f68ea5ce937507ba1a9c619cf760
darwisy01 <darwisy01@gmail.com>
2020-05-03 14:43:49 +0800

    added code

--- Mini_IT_Project/GUI_Planner.py
+++ Mini_IT_Project/GUI_Planner.py
@@ -1,13 +1,13 @@
 # Raven
-from tkinter import Frame, label
+from tkinter import Frame, Label
 import Constants
 
 class GUI_Planner:
 
     def __init__(self, parent: Frame):
-        self.plannerDesc = label(master = parent, text = "This platform all about your budget planner", font = ("", 24), bg = Constants.mainWindowBgColor)
+        self.plannerDesc = Label(master = parent, text = "This platform all about your budget planner", font = ("", 24), bg = Constants.mainWindowBgColor)
         self.plannerDesc.place(relx = 100 , rely = 100, anchor = "n")
-        self.plannerDesc2 = label(master = parent, text = "The safe way to double your money is to foldd it over once and put it in your pocket", font = ("", 24), bg = Constants.mainWindowBgColor)
+        self.plannerDesc2 = Label(master = parent, text = "The safe way to double your money is to foldd it over once and put it in your pocket", font = ("", 24), bg = Constants.mainWindowBgColor)
         self.plannerDesc2.place(relx = 0, rely = 0, anchor = "nw")
         
         pass
