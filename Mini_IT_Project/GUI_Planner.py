6c13a794f836c4d3010132b4f7ee4ea1a0bae747
darwisy01 <darwisy01@gmail.com>
2020-05-03 14:36:29 +0800

    desc

--- Mini_IT_Project/GUI_Planner.py
+++ Mini_IT_Project/GUI_Planner.py
@@ -6,8 +6,8 @@ class GUI_Planner:
 
     def __init__(self, parent: Frame):
         self.plannerDesc = label(master = parent, text = "This platform all about your budget planner", font = ("", 24), bg = Constants.mainWindowBgColor)
-        self.plannerDesc.place(x = 600, y = 300, anchor = "n")
+        self.plannerDesc.place(x = 600, y = 300, anchor = "north")
         self.plannerDesc2 = label(master = parent, text = "The safe way to double your money is to foldd it over once and put it in your pocket", font = ("", 24), bg = Constants.mainWindowBgColor)
-        self.plannerDesc2.place(x = 600, y = 360, anchor = "s")
+        self.plannerDesc2.place(x = 600, y = 360, anchor = "south")
         
         pass
