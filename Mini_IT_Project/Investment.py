b3fc900118dc59a0d55951857b77c7bcc9600ae4
BuildTools <unconfigured@null.spigotmc.org>
2020-05-03 09:56:07 +0800

    ...

--- Mini_IT_Project/Investment.py
+++ Mini_IT_Project/Investment.py
@@ -1,11 +1,12 @@
 # Raven
-from tkinter import Frame
+from tkinter import Frame, Label, Button, Canvas, PhotoImage
 import Constants
 
 class Investment:
 
     def __init__(self, parent: Frame):
-        
+        self.investmentDesc = Label(master = parent, text = "InvestmentERROR", font = ("", 36), bg = Constants.mainWindowBgColor)
+        self.investmentDesc.place(anchor = "center")
         
 
         pass
