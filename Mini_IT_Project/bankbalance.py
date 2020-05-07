cb32cf13a91a710ea23d42d6e48e049b726e5859
zhengwei210 <fongzhengwei@gmail.com>
2020-05-07 16:05:39 +0800

    bankaccount

--- Mini_IT_Project/bankbalance.py
+++ Mini_IT_Project/bankbalance.py
@@ -12,18 +12,3 @@ class BankAccount():
         self.amount = self.amount - money
 
 #over here is out of class, note indentation
-
-
-
-myAcc = BankAccount(100)   # Object creation
-
-myAcc.DepositMoney(20)      #So myAcc got 20 money
-
-fongsAcc = BankAccount(100)  # Object creation
-
-fongsAcc.DepositMoney(100)    #Add money to Fong Account
-
-fongsAcc.WithdrawMoney(10) #Taxes
-
-print(myAcc.amount)
-print(fongsAcc.amount)
