994e624530fd97ee5cac4640ae18a1086bd6930b
darwisy01 <darwisy01@gmail.com>
2020-05-10 17:48:38 +0800

    added code

--- Mini_IT_Project/GUI_Stats.py
+++ Mini_IT_Project/GUI_Stats.py
@@ -1,7 +1,19 @@
 from tkinter import Frame
+import GUI_Account
+import Account
+import Interfaces
 
 class Statistics():
 
     def __init__(self, parent: Frame):
         print("Console test - Statistics Init with frame: ", parent)
+        self.checker = GUI_Account.GUI_Account(self, parent)
+        burger1 = self.checker.accounts
+        burger2 = self.checker.categories
+        burger3 = self.checker.selectedAccount
+        burger4 = Account.Account.GetBalance(self)
+
+        print(burger1, burger2, burger3, burger4)
+
         pass
+    
