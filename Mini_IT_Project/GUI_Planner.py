0e587fc5339b21d2a4a66977a697fd1b301e638a
Raven Lim <ravenlimzhexuan1@gmail.com>
2020-05-10 13:30:37 +0800

    Working on Planner

--- Mini_IT_Project/GUI_Planner.py
+++ Mini_IT_Project/GUI_Planner.py
@@ -26,24 +26,23 @@ class GUI_Planner:
 
         #Calendar
         self.LCalendar     = Label(master = self.calendarFrame, text = "Date:", bg = Constants.mainWindowAltColor)
-        self.transCalendar = Calendar(self.calendarFrame, 
+        self.planCalendar = Calendar(self.calendarFrame, 
                                       font = "Arial 30", selectmode = 'day', cursor = "hand2", 
                                       year = datetime.date.today().year, month = datetime.date.today().month, day = datetime.date.today().day)
 
-        self.transCalendar.grid(row = 0, column = 0)
+        self.planCalendar.grid(row = 0, column = 0)
 
         self.calendarFrame.place(x = 75, y = 75)
 
     def InitTimeEntry(self):
 
         self.timePickerFrame = Frame(master = self.parent)
-
-        self.LTime = Label(master = self.parent, text = "Time:")
-
         self.timePickerFrame.place(relx = 0.91, y = 75, anchor = "ne")
 
+        self.LTime = Label(master = self.parent, text = "Start Time:")
+        self.LTime.place(relx = 0.62, y = 55, anchor = "nw")
 
-        self.previousTime = ["0", "0", "0"]
+        self.previousTime = ["08", "00", "00"]
         self.SVTime = [StringVar(), StringVar(), StringVar()]
         self.EntryTime = [None] * 3
         self.LsplitTime = [
@@ -66,6 +65,7 @@ class GUI_Planner:
                 self.previousTime[svi] = (self.SVTime[svi].get())
             except ValueError:
                 self.SVTime[svi].set(self.previousTime[svi])
+            self.VerifyDTDiff()
 
         def ButtonAddMinusTime(id):
             convert3 = id // 2
@@ -82,10 +82,8 @@ class GUI_Planner:
                 if (int(self.SVTime[convert3].get()) > 0):
                     self.SVTime[convert3].set(str(int(self.SVTime[convert3].get()) - 1))
 
-
-
         for i in range(len(self.SVTime)):
-            self.SVTime[i].set("0")
+            self.SVTime[i].set(self.previousTime[i])
             self.SVTime[i].trace("w", lambda name, index, mode, i = i: TimeVerify(i))
             self.EntryTime[i] = Entry(master = self.timePickerFrame, textvariable = self.SVTime[i])
             
@@ -103,7 +101,100 @@ class GUI_Planner:
             self.EntryTime[i].grid(row = 2, column = i, padx = (2, 2), pady = (2, 2))
             self.LsplitTime[i].grid(row = 0, column = i)
 
+
+
+        self.timePickerFrame2 = Frame(master = self.parent)
+        self.timePickerFrame2.place(relx = 0.91, y = 225, anchor = "ne")
+
+        self.LTime2 = Label(master = self.parent, text = "End Time:")
+        self.LTime2.place(relx = 0.62, y = 205, anchor = "nw")
+
+        self.previousTime2 = ["16", "0", "0"]
+        self.SVTime2 = [StringVar(), StringVar(), StringVar()]
+        self.EntryTime2 = [None] * 3
+        self.LsplitTime2 = [
+            Label(master = self.timePickerFrame2, text = "  Hour  "),
+            Label(master = self.timePickerFrame2, text = " Minute "),
+            Label(master = self.timePickerFrame2, text = " Second ")]
+        self.buttonTimeChange2 = [None] * 6
+
+        def TimeVerify2(svi):
+            try:
+                if (svi == 0):
+                    if (int(self.SVTime2[svi].get()) >= 24):
+                        self.SVTime2[svi].set(str(23))
+                else:
+                    if (int(self.SVTime2[svi].get()) >= 60):
+                        self.SVTime2[svi].set(str(59))
+                if (int(self.SVTime2[svi].get()) <= -1):
+                    self.SVTime2[svi].set(str(0))
+                    print(self.SVTime2[svi].get())
+                self.previousTime2[svi] = (self.SVTime2[svi].get())
+            except ValueError:
+                self.SVTime2[svi].set(self.previousTime2[svi])
+            self.VerifyDTDiff()
+
+        def ButtonAddMinusTime2(id):
+            convert3 = id // 2
             
+            if (convert3 == 0):
+                upperLimit = 23
+            else:
+                upperLimit = 59
+
+            if (id % 2 == 0):
+                if (int(self.SVTime2[convert3].get()) < upperLimit):
+                    self.SVTime2[convert3].set(str(int(self.SVTime2[convert3].get()) + 1))
+            else:
+                if (int(self.SVTime2[convert3].get()) > 0):
+                    self.SVTime2[convert3].set(str(int(self.SVTime2[convert3].get()) - 1))
+
+        for i in range(len(self.SVTime)):
+            self.SVTime2[i].set(self.previousTime2[i])
+            self.SVTime2[i].trace("w", lambda name, index, mode, i = i: TimeVerify2(i))
+            self.EntryTime2[i] = Entry(master = self.timePickerFrame2, textvariable = self.SVTime2[i])
+            
+            for z in range(2):
+                j = i*2 + z
+                self.buttonTimeChange2[j] = Button(master = self.timePickerFrame2, command = lambda i = j: ButtonAddMinusTime2(i))
+                if (z == 0):
+                    self.buttonTimeChange2[j].configure(text = "      +      ")
+                    r = 1
+                else:
+                    self.buttonTimeChange2[j].configure(text = "      -      ")
+                    r = 3
+                self.buttonTimeChange2[j].grid(row = r, column = i)
+
+            self.EntryTime2[i].grid(row = 2, column = i, padx = (2, 2), pady = (2, 2))
+            self.LsplitTime2[i].grid(row = 0, column = i)
+    
+    def VerifyDTDiff(self):
+        self.RetrieveDateTime()
+        if self.rEndDT < self.rStartDT:
+            for i in self.EntryTime:
+                i.configure(fg = "red")
+            for i in self.EntryTime2:
+                i.configure(fg = "red")
+            ######################## PUT BUTTON UNABLE TO CLICK ###############################
+        else:
+            for i in self.EntryTime:
+                i.configure(fg = "black")
+            for i in self.EntryTime2:
+                i.configure(fg = "black")
 
 
+    def RetrieveDateTime(self):
+        date = self.planCalendar.selection_get()
         
+        startTime = self.ConvertToTime(self.SVTime[0].get(), self.SVTime[1].get(), self.SVTime[2].get())
+        endTime = self.ConvertToTime(self.SVTime2[0].get(), self.SVTime2[1].get(), self.SVTime2[2].get())
+
+        self.rStartDT = datetime.datetime.combine(date, startTime)
+        self.rEndDT = datetime.datetime.combine(date, endTime)
+
+    def ConvertToTime(self, hour, minute, second):
+        return datetime.time(int(hour), int(minute), int(second))
+
+
+    def CompareStartEndTime(self):
+        pass
--- Mini_IT_Project/Plans.py
+++ Mini_IT_Project/Plans.py
@@ -1,15 +1,80 @@
+from __future__ import print_function
 import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 
 class Plans():
 
     def __init__(self):
-        pass
+
+        self.InitGCalendar()
+
+    def InitGCalendar(self):
+        SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+        creds = None
+
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+                
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        self.service = build('calendar', 'v3', credentials=creds)
 
 class Plan():
 
-    date = None
-    time = None
+    title = ""
+    location = ""
+    description = ""
+    frequency = "MONTHLY"
 
-    def __init__(self):
-        pass
+    def __init__(self, service):
+        self.startDT = None
+        self.endDT = None
+        self.stopDT = None
+        self.service = service
+
+    def SetGEvent(self):
+
+        self.stopDT = self.stopDT - datetime.timedelta(hours=8)
+
+        startTime = self.startDT.strftime("%y-%m-%dT%H:%M:%S")
+        endTime = self.endDT.strftime("%y-%m-%dT%H:%M:%S")
+        stopTime = datetime.datetime().now().strftime("%y%m%dT%H%M%SZ")
+
+        event = {
+        'summary': self.title,
+        'location': self.location,
+        'description': self.description,
+        'start': {
+            'dateTime': startTime,
+            'timeZone': 'Malaysia',
+        },
+        'end': {
+            'dateTime': endTime,
+            'timeZone': 'Malaysia',
+        },
+        'recurrence': [
+            'RRULE:FREQ='+ self.frequency +';UNTIL=' + stopTime
+        ],
+        'reminders': {
+            'useDefault': True
+        },
+        }
+
+        event = self.service.events().insert(calendarId='primary', body=event).execute()
+        print ('Event created: %s' % (event.get('htmlLink')))
